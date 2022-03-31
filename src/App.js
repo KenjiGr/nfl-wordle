81bc60c990da96bedcd8d3f0952c7fc6167e4467
KenjiGr <kenjigrr@gmail.com>
2022-03-31 17:12:44 +0200

    tailwind init

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mx-auto w-96'>
+        <h1 className='text-lg text-center'>Nfl Wordle</h1>
     </div>
   );
 }

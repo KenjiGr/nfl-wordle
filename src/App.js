ae7ee1229ce21b4b422351e267d7513718b0ac97
KenjiGr <kenjigrr@gmail.com>
2022-03-31 19:32:39 +0200

    Getting individual player data with axios call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+  const [player, setPlayer] = useState([])
+  const [error, setError] = useState([])
+  let key = '841c2605c6da4040a45c4de11797f56b';
+
+  const handleClick = () => {
+    axios.get(`https://api.sportsdata.io/v3/nfl/scores/json/Player/22493?key=${key}`).then(res => {
+      // console.log(res)
+      setPlayer(res.data)
+      console.log(res.data)
+    }).catch(err => {
+      setError(err)
+    })
+  }
+
+  // useEffect(() => {
+  //   axios.get(`https://api.sportsdata.io/v3/nfl/scores/json/Players/SF?key=${key}`).then(res => {
+  //     console.log(res.data)
+  //     setPlayer(res.data)
+  //   }).catch(err => {
+  //     setError(err)
+  //   })
+  // }, [])
+
   return (
     <div className='mx-auto w-96'>
         <h1 className='text-lg text-center'>Nfl Wordle</h1>
+        <h2>{player.Name}</h2>
+        <h2>{player.Age}</h2>
+        <h2>{player.CurrentTeam}</h2>
+        <h2>{player.Height}</h2>
+        <h2>{player.Position}</h2>
+        <h2>{player.Number}</h2>
+        <button onClick={handleClick}>Get player</button>
+        {/* <{!player : <h2>{error.message}</h2>}> */}
     </div>
   );
 }
